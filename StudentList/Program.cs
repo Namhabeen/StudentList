58fe3ee5284b5364755b188ae47d3475a925b7e0
South_under_been <s2018s27@e-mirim.hs.kr>
2020-05-19 19:37:57 +0900

    초기 커밋.

---
+++ StudentList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StudentList
+{
+    class Program
+    {
+        class Teacher
+        {
+            public String name;
+            public String subject;
+
+        }
+        static void Main(string[] args)
+        {
+            List<Teacher> list = new List<Teacher>();
+            list.Add(new Teacher() { name = "남하빈", subject = "뉴미디어콘텐츠제작" });
+            list.Add(new Teacher() { name = "남치코", subject = "귀여움" });
+            list.Add(new Teacher() { name = "함기훈", subject = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "이규응", subject = "컴퓨터구조" });
+            list.Add(new Teacher() { name = "우호식", subject = "수학" });
+            list.Add(new Teacher() { name = "이은주", subject = "영어" });
+
+            foreach(var item in list)
+            {
+                Console.Write(item.name+":"+item.subject);
+            }
+
+        }
+    }
+}
---
+++ StudentList/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList
+{
+    class Student
+    {
+        public String id;
+        public String name;
+        public int grade;
+        public String major;
+        public DateTime birthday;
+
+    }
+}
